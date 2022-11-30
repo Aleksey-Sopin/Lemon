a85501da197f30ab414b827630c728e80c18f491
Aleksey-Sopin <80095261+Aleksey-Sopin@users.noreply.github.com>
2022-11-30 20:28:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let n1 = document.querySelector(".subscribe-questions-block-question-1 span");
-let n2 = document.querySelector(".subscribe-questions-block-question-2 span");
-let n3 = document.querySelector(".subscribe-questions-block-question-3 span");
-let n4 = document.querySelector(".subscribe-questions-block-question-4 span");
+// let n1 = document.querySelector("#answer-1");
+// let n2 = document.querySelector("#answer-2");
+// let n3 = document.querySelector("#answer-3");
+// let n4 = document.querySelector("#answer-4");
+let answer = document.querySelector("#answer");
 
 let bodyBlock = document.querySelector("body");
 // блоки вопросов убираем правый маргин
@@ -17,83 +18,93 @@ subscribeQuestionsBlockQquestion_3.style.marginRight = "0px";
 let subscribeQuestionsBlockQquestion_4 = document.querySelector(".subscribe-questions-block-question-4");
 subscribeQuestionsBlockQquestion_4.style.marginRight = "0px";
 
-// 
+// subscribe-questions-block-question
 let question1 = document.querySelector(".subscribe-questions-block-question-1");
-question1.onclick = function () {
-	if (n1.style.display == "block") {
-		n1.style.display = "none";
-		question1.classList.toggle("activeBlock");
-		question1.style.height = "60px";
-	} else {
-		n1.style.display = "block";
-		question1.classList.toggle("activeBlock");
-		question1.style.height = "350%";
-		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
-			question1.style.height = "230px";
-		}
-	}
-} 
 let question2 = document.querySelector(".subscribe-questions-block-question-2");
-question2.onclick = function () {
-	if (n2.style.display == "block") {
-		n2.style.display = "none";
-		question2.classList.toggle("activeBlock");
-		question2.style.height = "60px";
-	} else {
-		n2.style.display = "block";
-		question2.classList.toggle("activeBlock");
-		question2.style.height = "350%";
-		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
-			question2.style.height = "230px"
-		}
-	}
-} 
 let question3 = document.querySelector(".subscribe-questions-block-question-3");
-question3.onclick = function () {
-	if (n3.style.display == "block") {
-		n3.style.display = "none";
-		question3.classList.toggle("activeBlock");
-		question3.style.height = "60px";
-	} else {
-		n3.style.display = "block";
-		question3.classList.toggle("activeBlock");
-		question3.style.height = "350%";
-		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
-			question3.style.height = "230px";
-		}
-	}
-} 
 let question4 = document.querySelector(".subscribe-questions-block-question-4");
-question4.onclick = function () {
-	if (n4.style.display == "block") {
-		n4.style.display = "none";
-		question4.classList.toggle("activeBlock");
-		question4.style.height = "60px";
-	} else {
-		n4.style.display = "block";
-		question4.classList.toggle("activeBlock");
-		question4.style.height = "350%";
-		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
-			question4.style.height = "215px";
-		} 
-	}
-} 
+
 // subscribe questions block chenge size
-if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769) {
-	question1.style.width = "88%";
-	question2.style.width = "88%";
-	question3.style.width = "88%";
-	question4.style.width = "88%";
+// if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769) {
+// 	question1.style.width = "88%";
+// 	question2.style.width = "88%";
+// 	question3.style.width = "88%";
+// 	question4.style.width = "88%";
+// 	} else if (bodyBlock.clientWidth < 768) {
 
-} else if (bodyBlock.clientWidth < 768) {
+// 	let subscribeQuestionsBlock = document.querySelector(".subscribe-questions");
+// 	subscribeQuestionsBlock.style.width = "100%";
 
-	let subscribeQuestionsBlock = document.querySelector(".subscribe-questions");
-	subscribeQuestionsBlock.style.width = "100%";
+// 	question1.style.width = "100%";
+// 	question2.style.width = "100%";
+// 	question3.style.width = "100%";
+// 	question4.style.width = "100%";
+// }
 
-	question1.style.width = "100%";
-	question2.style.width = "100%";
-	question3.style.width = "100%";
-	question4.style.width = "100%";
-}
-// 	let subscribeQuestionsBlock = document.querySelector("subscribe-questions");
-// subscribeQuestionsBlock.style.width = "90" + "%";
+
+// question1.addEventListener('click', quest_1);
+// question2.addEventListener('click', quest_2);
+// question3.addEventListener('click', quest_3);
+// question4.addEventListener('click', quest_4);
+
+
+
+// function quest_1(){
+// 	if (answer.style.display == "block") {
+// 		answer.style.display = "none";
+// 		question1.classList.toggle("activeBlock");
+// 		question1.style.height = "60px";
+// 	} else {
+// 		answer.style.display = "block";
+// 		question1.classList.toggle("activeBlock");
+// 		question1.style.height = "350%";
+// 		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
+// 			question1.style.height = "230px";
+// 		}
+// 	}
+// }  
+// function quest_2() {
+// 	if (answer.style.display == "block") {
+// 		answer.style.display = "none";
+// 		question2.classList.toggle("activeBlock");
+// 		question2.style.height = "60px";
+// 	} else {
+// 		answer.style.display = "block";
+// 		question2.classList.toggle("activeBlock");
+// 		question2.style.height = "350%";
+// 		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
+// 			question2.style.height = "230px"
+// 		}
+// 	}
+// }
+// function quest_3() {
+// 	if (answer.style.display == "block") {
+// 		answer.style.display = "none";
+// 		question3.classList.toggle("activeBlock");
+// 		question3.style.height = "60px";
+// 	} else {
+// 		answer.style.display = "block";
+// 		question3.classList.toggle("activeBlock");
+// 		question3.style.height = "350%";
+// 		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
+// 			question3.style.height = "230px";
+// 		}
+// 	}
+
+// } 
+// function quest_4() {
+// 	if (answer.style.display == "block") {
+// 		answer.style.display = "none";
+// 		question4.classList.toggle("activeBlock");
+// 		question4.style.height = "60px";
+// 	} else {
+// 		answer.style.display = "block";
+// 		question4.classList.toggle("activeBlock");
+// 		question4.style.height = "350%";
+// 		if( bodyBlock.clientWidth < 1545 && bodyBlock.clientWidth > 769){
+// 			question4.style.height = "215px";
+// 		} 
+// 	}
+	
+// }
+
